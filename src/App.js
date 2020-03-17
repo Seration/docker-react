6f8e2f0bbf3dccf61d77cd6dc5bdc44ccc221959
Sercan <s.selcuk23@gmail.com>
2020-03-17 21:05:45 +0300

    text changed again and again

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there from feature branch
+          Hi there from feature branch :(
         </a>
       </header>
     </div>
