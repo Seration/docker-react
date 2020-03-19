2feb8aa4b5a77d03fec42a1c89ba87f5f49cff2c
Sercan <s.selcuk23@gmail.com>
2020-03-19 19:59:51 +0300

    text changes in master

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React X
+          Learn React XYZ
         </a>
       </header>
     </div>
